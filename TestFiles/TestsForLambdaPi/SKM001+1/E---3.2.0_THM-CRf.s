7f0caa2a7b1a63098c2c7cd17b93ca1ecdf7ac03
Geoff Sutcliffe <2geoff@pobox.com>
2024-07-18 12:06:26 -0400

    Added assumed ASk axioms

--- TestFiles/TestsForLambdaPi/SKM001+1/E---3.2.0_THM-CRf.s
+++ TestFiles/TestsForLambdaPi/SKM001+1/E---3.2.0_THM-CRf.s
@@ -25,35 +25,48 @@ fof(c_0_3,negated_conjecture,
       | ~ q(a,X7,a,X8) ),
     inference(fof_nnf,[status(thm)],[inference(variable_rename,[status(thm)],[inference(fof_nnf,[status(thm)],[c_0_2])])]) ).
 
+fof(a1_ASk,axiom,
+    ! [X1] :
+      ( p(X1,esk1_1(X1))
+      & ! [X3] :
+        ? [X4] : q(X1,esk1_1(X1),X3,X4) ),
+    introduced(assumption) ).
+
+% tff(a1_epsilon,definition,
+%     ! [X1] :
+%       ( esk1_1(X1)
+%       = # [X2] : 
+%           ( p(X1,esk1_1(X1))
+%           & ! [X3] :
+%             ? [X4] : q(X1,X2,X3,X4) ) ),
+%     introduced(epsilon,[skolemized(X2),parent(a1)]) ).
+
 fof(c_0_4a,plain,
     ! [X1] :
       ( p(X1,esk1_1(X1))
       & ! [X3] :
         ? [X4] : q(X1,esk1_1(X1),X3,X4) ),
-    inference(skolemize,[status(esa),new_symbols(skolem,[esk1_1]),skolemized(X2)],[a1]) ).
+    inference(skolemize,[status(esa),new_symbols(skolem,[esk1_1]),skolemized(X2)],[a1,a1_ASk]) ).
 
-tff(c_0_4a_epsilon,definition,
+fof(c_0_4a_ASk,axiom,
     ! [X1] :
-      ( esk1_1(X1)
-      = # [X2] : 
-          ( p(X1,esk1_1(X1))
-          & ! [X3] :
-            ? [X4] : q(X1,X2,X3,X4) ) ),
-    introduced(epsilon,[skolemized(X2),parent(c_0_4a)]) ).
+      ( p(X1,esk1_1(X1))
+      & ! [X3] : q(X1,esk1_1(X1),X3,esk2_2(X1,X3)) ),
+    introduced(assumption) ).
+
+% tff(c_0_4a_epsilon,definition,
+%     ! [X1,X3] :
+%       ( esk2_2(X1,X3)
+%       = # [X4] :
+%           ( p(X1,esk1_1(X1))
+%           & q(X1,esk1_1(X1),X3,X4) ) ),
+%     introduced(epsilon,[skolemized(X4),parent(c_0_4a)]) ).
 
 fof(c_0_4,plain,
     ! [X1] :
       ( p(X1,esk1_1(X1))
       & ! [X3] : q(X1,esk1_1(X1),X3,esk2_2(X1,X3)) ),
-    inference(skolemize,[status(esa),new_symbols(skolem,[esk1_1]),skolemized(X4)],[c_0_4a]) ).
-
-tff(c_0_4_epsilon,definition,
-    ! [X1,X3] :
-      ( esk2_2(X1,X3)
-      = # [X4] :
-          ( p(X1,esk1_1(X1))
-          & q(X1,esk1_1(X1),X3,X4) ) ),
-    introduced(epsilon,[skolemized(X4),parent(c_0_4)]) ).
+    inference(skolemize,[status(esa),new_symbols(skolem,[esk1_1]),skolemized(X4)],[c_0_4a,c_0_4a_ASk]) ).
 
 cnf(c_0_5,negated_conjecture,
     ( ~ p(a,X1)
